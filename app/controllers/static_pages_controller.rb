9884d383d803999a5fd65b26b76a6070e0a95b53
dungvnk1 <vunangdungdta@gmail.com>
2024-08-09 09:32:08 +0700

    Remove unecessary :user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @micropost  = current_user.microposts.includes(:user, :image_attachment).build
+      @micropost  = current_user.microposts.includes(:image_attachment).build
       @feed_items = current_user.feed.includes(:image_attachment).paginate(page: params[:page])
     end
   end
